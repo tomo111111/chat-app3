726fc65e25bc93760905bf1cd3291df64c820074
tomo111111 <ttt0206sss@gmail.com>
2020-08-09 14:16:10 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+  def create
+    @message = Message.create(message_params)
+    if @message.save
+      redirect_to room_messages_path(@message.room_id)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(room_id:params[:room_id],user_id:current_user.id)
   end
 end
